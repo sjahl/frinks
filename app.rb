973af6edfd96d48eaf210be7858b7c9ad2147764
Stephen Jahl <stephenjahl@gmail.com>
2012-02-20 18:49:47 -0500

    datamapper's back in

--- app.rb
+++ app.rb
@@ -4,10 +4,25 @@
 # Description: a Sinatra app for storing links in a database
 
 require 'sinatra'
+require 'yaml'
 require 'data_mapper'
 
 # Database
-#
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/frinks.db")
+
+class FrinkLink
+  include DataMapper::Resource
+  property :id, Serial
+  property :title, String
+  property :url, String
+  property :created_at, DateTime
+  property :is_starred, Boolean
+end
+
+DataMapper.finalize
+
+FrinkLink.auto_upgrade!
+
 # Visible Routes
 get '/' do
   "Hello World"
