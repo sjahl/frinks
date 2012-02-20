7e67705c5de97ad40c8156383ded2120f8d84416
Stephen Jahl <stephenjahl@gmail.com>
2012-02-20 18:12:21 -0500

    Added inital db support

--- app.rb
+++ app.rb
@@ -4,11 +4,25 @@
 # Description: a Sinatra app for storing links in a database
 
 require 'sinatra'
+require 'data_mapper'
 
+# Database
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/frinks.db")
+
+class Link
+  include DataMapper::Resource
+  property :id, Serial
+  property :name, String
+  property :url, Text
+  property :created_at, DateTime
+end
+
+# Visible Routes
 get '/' do
   "Hello World!"
 end
 
+# API 
 get '/api/time' do
   "#{Time.now}"
 end
