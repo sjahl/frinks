3409b9be68fde8afb28ec6c6ea07c266b74ac912
sjahl <stephenjahl+git@gmail.com>
2012-02-24 20:04:22 -0500

    Some comments in config.rb

--- config.rb
+++ config.rb
@@ -1,3 +1,6 @@
 #!/usr/bin/env ruby
+# Thought this might be nice as a hash in a separate file...
+# might have more to configure later.
+
 
 DATABASE = { database_path: "#{ENV['HOME']}/frinks.db" }
